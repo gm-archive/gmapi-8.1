f270b06e44cff094e1eba8e29780c4a133b8ed2e
SyncViews <wnewbery@hotmail.co.uk>
2012-08-07 14:02:25 +0000

    - Corrected gm::Shared::hookVersion type.
    git-svn-id: http://svn.willnewbery.com/gmapi/trunk@26 86a89a20-5807-464c-944c-25d075e0f2c0

--- Source/CoreInternal.hpp
+++ Source/CoreInternal.hpp
@@ -1,21 +1,21 @@
 /* Copyright (c) 2011-2012 William Newbery
- * 
+ *
  * This software is provided 'as-is', without any express or implied
  * warranty. In no event will the authors be held liable for any damages
  * arising from the use of this software.
- * 
+ *
  * Permission is granted to anyone to use this software for any purpose,
  * including commercial applications, and to alter it and redistribute it
  * freely, subject to the following restrictions:
- * 
+ *
  * 1. The origin of this software must not be misrepresented; you must not
  * claim that you wrote the original software. If you use this software
  * in a product, an acknowledgment in the product documentation would be
  * appreciated but is not required.
- * 
+ *
  * 2. Altered source versions must be plainly marked as such, and must not be
  * misrepresented as being the original software.
- * 
+ *
  * 3. This notice may not be removed or altered from any source
  * distribution.
  */
@@ -37,7 +37,7 @@ namespace gm
         /**The version of the hook code. If a GMAPI instance is loaded after
          * another GMAPI was already loaded, but this one has a newer version
          * of the code, it will replace the old hook with its newer one.*/
-        int hookVersion;
+        unsigned hookVersion;
         /**The DLL that owns the current hook (to ensure that DLL can't get
          * unloaded while the hook is still in place).
          */
